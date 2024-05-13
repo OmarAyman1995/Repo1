321137a11f70c96b3c99f2d7c27b9700f6f7104a
OmarAyman1995 <omar.ayman@crewteq.com>
2024-05-13 22:26:47 +0300

    update C++

---
+++ C++/projects/Section13 (OOP)/OOP/OOP/OOP.cpp
@@ -0,0 +1,48 @@
+/*
+* Author		: Omar Ayman
+* Date			: 13th MAY 2024
+* Copyrights	: omar.ayman@crewteq.com
+*
+*
+* do NOT use this for adding history to track you code edits,
+* use the version control instead (GIT)
+*/
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+using std::cin; // use only what you need in the std name space 
+using std::cout;// use only what you need in the std name space 
+using std::endl;// use only what you need in the std name space 
+
+int main()
+{
+	/* what is OOP (Object Oriented Programming)
+	- focus on classes that model real world domain entities
+	- allows devlopers to think at a higher level of abstraction
+	- used successfully in very large programs 
+	- Encapsulation (information - hiding )
+	- reusability (easy to reuse classes in other applications)
+	- inheritance (create a new class in term of existing classes)
+
+	- programs can be : 
+	 1- large in size 
+	 2- slower 
+	 3- More complex
+
+	 * classes (blueprint from which objects are created)
+	 - has methods 
+	 - can hide data and methods 
+	 - provide public interface 
+	 - has attributes (data)
+	 Examples from standard library
+	 : 
+	  1- std::vector
+	  2- std::string
+	   
+	  * the object is an instance from the class
+
+	   
+	   */
+}
