0add8f5d5d210b61d3d9755657043326e05a5667
OmarAyman1995 <omar.ayman@crewteq.com>
2024-06-08 21:24:31 +0300

    Update C++ from home2

---
+++ C++/projects/Smart pointer/smart pointer/smart pointer/smart pointer.cpp
@@ -0,0 +1,16 @@
+/*
+* Author		: Omar Ayman
+* Date			: 8th June 2024
+* Copyrights	: omar.ayman@crewteq.com
+*
+*
+*/
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
